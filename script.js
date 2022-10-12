bb49e1961382e84787ef23fbda0ddf9f202a0271
Orange4Broom <106157524+Orange4Broom@users.noreply.github.com>
2022-10-12 12:43:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+function clock() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-document.getElementById("timer").innerHTML = time;
+    document.getElementById("timer").innerHTML = time; 
+}
+setInterval(clock, 1000);
